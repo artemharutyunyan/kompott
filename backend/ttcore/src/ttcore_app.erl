770537de4b7601dc4408de6b540690d419df1ebd
Artem Harutyunyan <artem@harutyunyan.am>
2014-10-07 01:43:39 -0700

    Initial comiTT

---
+++ backend/ttcore/src/ttcore_app.erl
@@ -0,0 +1,21 @@
+-module(ttcore_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", cowboy_static, {priv_file, ttcore, "static/index.html"}}]}
+    ]),
+    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [{env, [{dispatch, Dispatch}]}]),
+    ttcore_sup:start_link().
+
+stop(_State) ->
+    ok.
