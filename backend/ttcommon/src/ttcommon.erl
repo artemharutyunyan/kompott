a89e5cb991556b6b5f1c928354014a75a9ca8154
Artem Harutyunyan <artem@harutyunyan.am>
2014-10-29 23:51:58 -0700

    Adding ttcommon

---
+++ backend/ttcommon/src/ttcommon.erl
@@ -0,0 +1,34 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Module containing common functons
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(ttcommon).
+
+-export([format_cowboy_reply/1]).
+
+%% Formats result of a given function call for passing to cowboy_req:reply
+format_cowboy_reply(F) ->
+    try F() of
+        %% Return 200 if the call returned 'ok'
+        {ok, json, Json} ->
+            {200, convert(json, binary, Json)};
+        %% Return 500 if the call returned 'error' (or something unexpected)
+        {error, json, Json} ->
+            {500, convert(json, binary, Json)};
+        E ->
+            {500, format_string("~8192p", [E])}
+    catch
+        T:E ->
+          Stack = erlang:get_stacktrace(),
+          lager:error("Exception ~p:~p~nStack: ~p", [T, E, Stack]),
+          {500, format_string("Exception ~p:~p~nStack: ~8192p", [T, E, Stack])}
+    end.
+
+%% Internal function for performing conversions between various types
+convert(json, binary, J) ->
+    iolist_to_binary(jsx:encode(J)).
+
+%% Internal function for formatting binary strings
+format_string(Format, String) ->
+    list_to_binary(io_lib:format(Format, String)).
+
--- backend/ttfw/src/ttfw_packages.erl
+++ backend/ttfw/src/ttfw_packages.erl
@@ -11,10 +11,19 @@ init(_Transport, Request, []) ->
   {ok, Request, undefined}.
 
 handle(Request, State) ->
-  {_Method, Request1} = cowboy_req:method(Request),
+  {Method, Request1} = cowboy_req:method(Request),
   {ok, Request2} = cowboy_req:reply(200, [{<<"content-type">>, <<"text/plain">>}], <<"TT #ftw">>, Request1),
   {ok, Request2, State}.
 
 terminate(_Reason, _Request, _State) ->
   ok.
+
+%% Endpoint handlers
+packages(<<GET>>, Req) ->
+  {Status, Resp} = ttcore:format_cowboy_reply(fun() -> get_packages(Req) end),
+  cowboy_req:reply(Status, Resp).
+
+%% GET packages/
+get_packages(Req) ->
+    {ok, json, <<{}>>}.
 
