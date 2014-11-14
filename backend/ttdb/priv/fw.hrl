6e7b9ed2f116e79de02054194ef5d31854ffc7e1
Artem Harutyunyan <artem@harutyunyan.am>
2014-11-14 01:01:18 -0800

    Custtomers can be added to the database

--- backend/ttdb/priv/fw.hrl
+++ backend/ttdb/priv/fw.hrl
@@ -5,7 +5,9 @@
 %% --------------------------------------------------------------------------
 
 %% Constants
--define(CUSTOMER_BUCKET, <<tt_customer>>).
+-define(RIAK_HOST, "127.0.0.1").
+-define(RIAK_PORT, 8087).
+-define(CUSTOMER_BUCKET, <<"tt_customer">>).
 
 %% Records
 -record(customer, {name, uuid, date}).
--- backend/ttdb/src/fw.erl
+++ backend/ttdb/src/fw.erl
@@ -8,6 +8,13 @@
 
 -export([customer_add/1]).
 
-customer_add(#customer{} = _Customer) ->
-   ok.
+customer_add(#customer{} = Customer) ->
+   {ok, Pid} = get_riak_connection(),
+   UUID = uuid:get_v4(),
+   lager:info("Adding customer ~p (UUID: ~p).", [Customer#customer.name, uuid:uuid_to_string(UUID)]),
+   C = Customer#customer{uuid = uuid:uuid_to_string(UUID), date = ttcommon:utc_timestamp()},
+   CustomerObj = riakc_obj:new(?CUSTOMER_BUCKET, list_to_binary(uuid:uuid_to_string(UUID)), C),
+   ok = riakc_pb_socket:put(Pid, CustomerObj).
 
+get_riak_connection() ->
+   riakc_pb_socket:start_link(?RIAK_HOST, ?RIAK_PORT).
