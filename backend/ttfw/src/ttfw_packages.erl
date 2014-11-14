ba3140a5addd374a01dd8ba1f076a12cbbdf8e6a
Artem Harutyunyan <artem@harutyunyan.am>
2014-11-13 23:00:34 -0800

    Storing latest version

--- backend/ttfw/src/ttfw_packages.erl
+++ backend/ttfw/src/ttfw_packages.erl
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Module for handling requests to packages/ endpoint.  
+%% Module for handling requests to packages/ endpoint.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -module(ttfw_packages).
 
-%% Cowboy callbacks 
+%% Cowboy callbacks
 -export([init/3, handle/2, terminate/3]).
 
 init(_Transport, Request, []) ->
@@ -12,18 +12,18 @@ init(_Transport, Request, []) ->
 
 handle(Request, State) ->
   {Method, Request1} = cowboy_req:method(Request),
-  {ok, Request2} = cowboy_req:reply(200, [{<<"content-type">>, <<"text/plain">>}], <<"TT #ftw">>, Request1),
+  {ok, Request2} = packages(Method, Request1),
   {ok, Request2, State}.
 
 terminate(_Reason, _Request, _State) ->
   ok.
 
 %% Endpoint handlers
-packages(<<GET>>, Req) ->
+packages(<<"GET">>, Req) ->
   {Status, Resp} = ttcore:format_cowboy_reply(fun() -> get_packages(Req) end),
   cowboy_req:reply(Status, Resp).
 
 %% GET packages/
-get_packages(Req) ->
-    {ok, json, <<{}>>}.
+get_packages(_Req) ->
+    {ok, json, <<"{}">>}.
 
