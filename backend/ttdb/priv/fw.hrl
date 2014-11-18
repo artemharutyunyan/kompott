64dcc9c5de7754617bbec085accbdaef6b445589
Artem Harutyunyan <artem@harutyunyan.am>
2014-11-18 01:21:13 -0800

    Adding support for creating releases.

--- backend/ttdb/priv/fw.hrl
+++ backend/ttdb/priv/fw.hrl
@@ -13,12 +13,15 @@
 -define(CUSTOMER_BUCKET, <<"tt_customer">>).
 -define(DEVICE_BUCKET, <<"tt_device">>).
 -define(PACKAGE_BUCKET, <<"tt_package">>).
+-define(RELEASE_BUCKET, <<"tt_release">>).
 
 -define(CUSTOMER_DEVICE_SET_BUCKET, {<<"sets">>, <<"tt_customer_devices">>}).
 -define(DEVICE_PACKAGE_SET_BUCKET, {<<"sets">>, <<"tt_device_packages">>}).
+-define(PACKAGE_RELEASE_SET_BUCKET, {<<"sets">>, <<"tt_package_releases">>}).
 
 %% Records
 -record(customer, {name, uuid, creation_time}).
 -record(device, {name, uuid, creation_time, update_time}).
 -record(package, {name, uuid, creation_time, update_time, latest_release}).
+-record(release, {name, uuid, creation_time, version, files}).
 
--- backend/ttdb/src/fw.erl
+++ backend/ttdb/src/fw.erl
@@ -6,7 +6,7 @@
 -module(fw).
 -include("priv/fw.hrl").
 
--export([customer_add/1, device_add/2, package_add/3]).
+-export([customer_add/1, device_add/2, package_add/3, release_add/4]).
 
 %%
 %% Public functions
@@ -42,18 +42,40 @@ package_add(#customer{} = Customer, #device{} = Device, #package{} = Package) ->
     %% Create a package and add it to the package bucket
     UUID  = uuid:uuid_to_string(uuid:get_v4()),
     lager:info("Adding package ~p (UUID ~p) for device ~p, customer ~p.",
-                [Package#package.name, UUID, Device#device.uuid, Device#device.uuid]),
+        [Package#package.name, UUID, Device#device.uuid, Device#device.uuid]),
     P = Package#package{uuid = UUID, creation_time = ttcommon:utc_timestamp()},
     %% Add package to packages bucket
     PackageObj = riakc_obj:new(?PACKAGE_BUCKET, package_key(Customer, Device, P), P),
     ok = riakc_pb_socket:put(Pid, PackageObj),
     %% Add package to device's set of packages
     ok = add_package_to_device_set(Customer, Device, P).
+
+release_add(#customer{} = Customer, #device{} = Device, #package{} = Package) ->
+    {ok, Pid} = get_riak_connection(),
+    %% Create a release and add it to the release bucket
+    UUID = uuid:uuid_to_string(uuid:get_v4()),
+    lager:info("Adding release ~p (UUID ~p) for package ~p, device ~p, customer ~p.",
+        [Release#release.name, UUID, Package#package.uuid, Device#device.uuid, Customer#customer.uuid]),
+    R = Release#release{uuid = UUID, creation_time = ttcommon:utc_timestamp()},
+    %% Add release to releases bucket
+    ReleaseObj = riakc_obj:new(?RELEASE_BUCKET, release_key(Customer, Device, Package, R),
+    ok = riakc_pb_socket:put(Pid, ReleaseObj),
+    %% Add a release to package's set of releases
+    ok = add_release_to_package_set(Customer, Device, Package, R).
+
 %%
 %% Internal functions
 %%
 
-%% Auxiliary function for adding package to device's package set
+%% Auxiliary function for adding a release to to package's release set
+add_release_to_package_set(#customer{} = Customer, #device{} = Device, #package{} = Package, #release{} = Release) ->
+    {ok, Pid} = get_riak_connection(),
+    S = get_package_release_set(Customer, Device, Package),
+    Set = riakc_set:add_element(release_key(Customer, Device, Package, Release), S),
+    Key = package_release_set_key(Customer, Device, Package),
+    ok = riakc_pb_socket:update_type(Pid, ?PACKAGE_RELEASE_SET_BUCKET, Key, riakc_set:to_op(Set)).
+
+%% Auxiliary function for adding a package to device's package set
 add_package_to_device_set(#customer{} = Customer, #device{} = Device, #package{} = Package) ->
     {ok, Pid} = get_riak_connection(),
     S = get_device_package_set(Customer, Device),
@@ -61,7 +83,33 @@ add_package_to_device_set(#customer{} = Customer, #device{} = Device, #package{}
     Key = device_package_set_key(Customer, Device),
     ok = riakc_pb_socket:update_type(Pid, ?DEVICE_PACKAGE_SET_BUCKET, Key, riakc_set:to_op(Set)).
 
-%% Auxiliary function for retrieving the bucket set where device's releases are kept
+%% Auxiliary function for adding device to customer's device bucket set
+add_device_to_customer_set(#customer{} = Customer, #device{} = Device) ->
+    {ok, Pid} = get_riak_connection(),
+    S = get_customer_device_set(Customer),
+    Set = riakc_set:add_element(device_key(Customer, Device), S),
+    Key = customer_device_set_key(Customer),
+    ok = riakc_pb_socket:update_type(Pid, ?CUSTOMER_DEVICE_SET_BUCKET, Key, riakc_set:to_op(Set)).
+
+
+%% Auxiliary function for retrieving the bucket set where package's releases are kept
+get_package_release_set(#customer{} = Customer, #device{} = Device, #package{} = Package) ->
+    {ok, Pid} = get_riak_connection(),
+    Key = package_release_set_key(Customer, Device, Package),
+    %% Retrieve package's releases
+    case riakc_pb_socket:fetch_type(Pid, ?PACKAGE_RELEASE_SET_BUCKET, Key) of
+        {ok, Set} ->
+            %% Set exists
+            Set;
+        {error, {notfound, set}} ->
+            %% Set does not exist. Create an empty set an return it.
+            lager:info("Creating release set for package ~p, device ~p, customer ~p",
+                [Package#package.uuid, Device#device.uuid, Customer#customer,uuid]),
+        _ ->
+            throw(unexpected_fetch_type_return)
+    end.
+
+%% Auxiliary function for retrieving the bucket set where device's packages are kept
 get_device_package_set(#customer{} = Customer, #device{} = Device) ->
     {ok, Pid} = get_riak_connection(),
     Key = device_package_set_key(Customer, Device),
@@ -71,21 +119,13 @@ get_device_package_set(#customer{} = Customer, #device{} = Device) ->
             %% Set exists
             Set;
         {error, {notfound, set}} ->
-            %% Set does not exist. Create an empty set and return
+            %% Set does not exist. Create an empty set and return it.
             lager:info("Creating release set for device ~p, customer ~p.",
                        [Device#device.uuid, Customer#customer.uuid]),
             riakc_set:new();
         _ ->
             throw(unexpected_fetch_type_return)
     end.
-
-%% Auxiliary function for adding device to customer's device bucket set
-add_device_to_customer_set(#customer{} = Customer, #device{} = Device) ->
-    {ok, Pid} = get_riak_connection(),
-    S = get_customer_device_set(Customer),
-    Set = riakc_set:add_element(device_key(Customer, Device), S),
-    Key = customer_device_set_key(Customer),
-    ok = riakc_pb_socket:update_type(Pid, ?CUSTOMER_DEVICE_SET_BUCKET, Key, riakc_set:to_op(Set)).
 
 %% Auxiliary function for retrieving the bucket set where customer's devices are kept
 get_customer_device_set(#customer{} = Customer) ->
@@ -117,9 +157,15 @@ device_key(#customer{} = Customer, #device{} = Device) ->
 package_key(#customer{} = Customer, #device{} = Device, #package{} = Package) ->
     list_to_binary(Customer#customer.uuid ++ "_" ++ Device#device.uuid ++ "_" ++ Package#package.uuid).
 
+release_key(#customer{} = Customer, #device{} = Device, #package{} = Package, #release{} = Release) ->
+    list_to_binary(Customer#customer.uuid ++ "_" ++ Device#device.uuid ++ "_" ++ Package#package.uuid ++ "_" ++ Release#release.uuid).
+
 customer_device_set_key(#customer{} = Customer) ->
     list_to_binary(Customer#customer.uuid ++ "_device_set").
 
 device_package_set_key(#customer{} = Customer, #device{} = Device) ->
     list_to_binary(Customer#customer.uuid ++ "_" ++ Device#device.uuid ++ "_package_set").
+
+package_release_set_key(#customer{} = Customer, #device{} = Device, #package{} = Package) ->
+    list_to_binary(Customer#customer.uuid ++ "_" ++ Device#device.uuid ++ "_" ++ Package#package.uuid ++ "_release_set").
 
