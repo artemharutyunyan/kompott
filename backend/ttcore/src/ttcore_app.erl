e16d9586698128d21480199546b8e6213f616b46
Artem Harutyunyan <artem@harutyunyan.am>
2014-12-02 01:03:26 -0800

    - ADD: Proper REST handling for 'devices' endpoint - FIX: Endpoint path routing according to protocol spec

--- backend/ttcore/src/ttcore_app.erl
+++ backend/ttcore/src/ttcore_app.erl
@@ -12,7 +12,8 @@
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
         {'_', [
-                {"/", ttfw_packages, []}
+                {"/v1/customers/:customer/devices/[:deviceId]", ttfw_devices, []},
+                {"/v1/customers/:customer/devices/:deviceId/packages/[:packageId]", ttfw_packages, []}
               ]
         }
     ]),
---
+++ backend/ttfw/src/ttfw_devices.erl
@@ -0,0 +1,52 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Module for handling requests to devices/ endpoint.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(ttfw_devices).
+
+%% Cowboy callbacks
+-export([init/3]).
+-export([known_methods/2]).
+-export([allowed_methods/2]).
+-export([content_types_provided/2]).
+-export([content_types_accepted/2]).
+-export([is_authorized/2]).
+
+-export([devices_to_json/2]).
+-export([devices_from_json/2]).
+
+%%
+%% Cowboy callbacks
+init(_Transport, _Request, []) ->
+  {upgrade, protocol, cowboy_rest}.
+
+allowed_methods(Request, State) ->
+    {[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>, <<"POST">>], Request, State}.
+
+known_methods(Request, State) ->
+    {[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>, <<"POST">>], Request, State}.
+
+content_types_provided(Request, State) ->
+    {[
+        {<<"application/json">>, devices_to_json}
+     ],
+     Request, State}.
+
+content_types_accepted(Request, State) ->
+    {[
+        {<<"application/json">>, devices_from_json}
+     ],
+     Request, State}.
+
+is_authorized(Request, State) ->
+    {true, Request, State}.
+
+devices_to_json(Request, State) ->
+    Body = <<"\{\"result\":\"ok\"\}">>,
+    {Body, Request, State}.
+
+devices_from_json(Request, State) ->
+    lager:info("Reached here. Customer is: ~p,"),
+    {{<<"\{\}">>}, Request, State}.
+
+
